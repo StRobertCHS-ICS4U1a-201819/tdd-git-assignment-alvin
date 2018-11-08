df62f0876a8be6f3bfc8ef593c172c41e9b88213
Meitnerium109 <andrewlau44@yahoo.ca>
2018-11-07 23:52:04 -0500

    pass general case

--- statTools.py
+++ statTools.py
@@ -1,1 +1,5 @@
-
+def mean(my_list):
+    total = 0
+    for i in range(len(my_list)):
+        total += my_list[i]
+    return total/len(my_list)
--- test_statTools.py
+++ test_statTools.py
@@ -1,3 +1,5 @@
 import pytest
 from statTools import *
 
+def test_mean_basic1():
+    assert(mean([4,1,7,2,8]) == 4.4)
