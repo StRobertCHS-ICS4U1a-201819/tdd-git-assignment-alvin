a4438b3bb95f0f0cf6944f4a52538e8db0147c70
Meitnerium109 <andrewlau44@yahoo.ca>
2018-11-10 21:14:44 -0500

    mean unusual case test pass

--- statTools.py
+++ statTools.py
@@ -1,7 +1,31 @@
+"""
+-------------------------------------------------------------------------------
+Name:		statTools.py
+Purpose:
+(Mean):
+Add all integers in a given list and divide the total by the amount of integers
+in the list. Return the result.
+
+Author:		Lau.A
+
+Created:		09/09/2018
+------------------------------------------------------------------------------
+"""
+
 def mean(my_list):
-    total = 0
-    if len(my_list) == 0:
-        return "Error: Empty List"
-    for i in range(len(my_list)):
-        total += my_list[i]
-    return total/len(my_list)
+    try:
+        total = 0
+        if len(my_list) == 0:
+            return "Error: Empty List"
+        for i in range(len(my_list)):
+            total += my_list[i]
+        return total/len(my_list)
+    except TypeError:
+        return "Error: TypeError"
+
+
+
+
+
+
+
--- test_statTools.py
+++ test_statTools.py
@@ -1,17 +1,27 @@
 import pytest
 from statTools import *
 
+#general case: sorted list
 def test_mean_basic1():
-    assert(mean([4,1,7,2,8]) == 4.4)
+    assert(mean([1,2,4,7,8]) == 4.4)
 
+#general case: unsorted list
 def test_mean_basic2():
     assert(mean([7,1,3,0,0,1]) == 2)
 
+#general case: list with negative integers included
 def test_mean_basic3():
     assert(mean([3,1,-4,1,-6,2]) == -0.5)
 
+#corner case: list of 1 integer
 def test_mean_corner1():
     assert(mean([0]) == 0)
 
+#corner case: empty list
 def test_mean_corner2():
     assert(mean([]) == "Error: Empty List")
+
+#unusual case: string input
+def test_mean_unusual():
+    assert(mean(["2","4","7","3"]) == "Error: TypeError")
+
