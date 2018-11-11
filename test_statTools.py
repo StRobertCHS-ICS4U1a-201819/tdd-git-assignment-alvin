4e679ad19d969f5d4557da52a72e884822a02d21
Meitnerium109 <andrewlau44@yahoo.ca>
2018-11-10 19:53:22 -0500

    mean corner case 2 test pass (previous mean general case 3 is actually corner case 2)

--- test_statTools.py
+++ test_statTools.py
@@ -8,10 +8,10 @@ def test_mean_basic2():
     assert(mean([7,1,3,0,0,1]) == 2)
 
 def test_mean_basic3():
-    assert(mean([]) == 0)
-
-def test_mean_basic4():
     assert(mean([3,1,-4,1,-6,2]) == -0.5)
 
 def test_mean_corner1():
     assert(mean([0]) == 0)
+
+def test_mean_corner2():
+    assert(mean([]) == "Error: Empty List")
