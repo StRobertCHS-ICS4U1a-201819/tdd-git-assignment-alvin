728bfbd2350b864a6aa281b63076918c03346f98
edmundru <edmund.ru2@gmail.com>
2018-11-11 15:31:43 -0500

    median

--- test_statTools.py
+++ test_statTools.py
@@ -1,4 +1,8 @@
 import pytest
 from statTools import*
 
+def test_median_basic1():
+    assert(median([1, 2, 3, 4, 5, 6, 7]) == 4)
 
+def test_median_basic2():
+    assert(median([9, 8, 7, 6, 5, 4, 3]) == 6)
