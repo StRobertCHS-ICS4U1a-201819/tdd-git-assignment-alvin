3c815b84277f89da06326fc605955085f291ddf1
edmundru <edmund.ru2@gmail.com>
2018-11-11 15:54:50 -0500

    median

--- statTools.py
+++ statTools.py
@@ -1,2 +1,15 @@
 def median(num):
 
+    num_list = list(num)
+    numbers = len(num_list)
+
+    if numbers % 2 == 1:
+        list_median = num_list[(int(numbers/2))]
+
+    elif numbers == 0:
+        list_median = 0
+
+    else:
+        list_median = (num_list[int(numbers/2) - 1] + num_list[int(numbers/2)])/2
+
+    return list_median
