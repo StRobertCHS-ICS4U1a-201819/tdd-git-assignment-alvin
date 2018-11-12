f5eb515ccc00725b30d359f6451449c2a0e056ff
edmundru <edmund.ru2@gmail.com>
2018-11-11 20:04:47 -0500

    all tests passed

--- statTools.py
+++ statTools.py
@@ -1,1 +1,93 @@
+import math
 
+
+def median(num):
+
+    if len(num) % 2 == 1:
+        num = num[(int(len(num)/2))]
+
+    elif len(num) == 0:
+        return "Error: Empty List"
+
+    else:
+        num = (num[int(len(num)/2) - 1] + num[int(len(num)/2)])/2
+
+    return num
+
+
+def ranges(num):
+
+    num_list = list(num)
+
+    if len(num_list) >= 2:
+        return num_list[len(num_list) - 1] - num_list[0]
+
+    elif len(num_list) == 1:
+        return num_list[0]
+
+    else:
+        return "Error: Empty List"
+
+
+def variance(list):
+
+    len_list = len(list)
+    list_mean_diff = []
+    list_squared = []
+
+    if len_list == 0:
+        return "Error: Empty List"
+
+    elif len_list == 1:
+        return "Error: List must have 2 or more integers"
+
+    else:
+        mean = sum(list) / len(list)
+        a = 0
+        while a <= len_list - 1:
+            x = list[a] - mean
+            list_mean_diff.append(x)
+            a = a + 1
+
+        b = 0
+        while b <= len_list - 1:
+            y = list_mean_diff[b] * list_mean_diff[b]
+            list_squared.append(y)
+            b = b + 1
+
+        sum_list_squared = sum(list_squared)
+        number = (sum_list_squared / len_list)
+
+        return number
+
+
+def standard_deviation(list):
+
+    len_list = len(list)
+    list_mean_diff = []
+    list_squared = []
+
+    if len_list == 0:
+        return "Error: Empty List"
+
+    elif len_list == 1:
+        return "Error: List must have 2 or more integers"
+
+    else:
+        mean = sum(list) / len(list)
+        a = 0
+        while a <= len_list - 1:
+            x = list[a] - mean
+            list_mean_diff.append(x)
+            a = a + 1
+
+        b = 0
+        while b <= len_list - 1:
+            y = list_mean_diff[b] * list_mean_diff[b]
+            list_squared.append(y)
+            b = b + 1
+
+        sum_list_squared = sum(list_squared)
+        number = (sum_list_squared / len_list)
+
+        return float((math.sqrt(number)))
--- test_statTools.py
+++ test_statTools.py
@@ -1,2 +1,58 @@
 import pytest
-from statTools import *
+from statTools import*
+
+# GENERAL MEDIAN TESTS
+def test_median_basic1():
+    assert(median([1, 2, 3, 4, 5, 6, 7]) == 4)
+
+def test_median_basic2():
+    assert(median([2, 4, 6, 7]) == 5)
+
+# CORNER MEDIAN TESTS
+def test_median_corner1():
+    assert(median([]) == "Error: Empty List")
+
+def test_median_corner2():
+    assert(median([0]) == 0)
+
+# GENERAL RANGE TESTS
+def test_range_basic1():
+    assert(ranges([1, 2, 3, 4, 5, 6]) == 5)
+
+def test_range_basic2():
+    assert(ranges([3, 4, 5, 5, 6, 7, 13]) == 10)
+
+# CORNER RANGE TESTS
+def test_range_corner1():
+    assert(ranges([5]) == 5)
+
+def test_range_corner2():
+    assert(ranges([]) == "Error: Empty List")
+
+# GENERAL VARIANCE TESTS
+def test_variance_basic1():
+    assert(variance([10, 20, 30, 40]) == 125)
+
+def test_variance_basic2():
+    assert(variance([600, 470, 170, 430, 300]) == 21704)
+
+# CORNER VARIANCE TESTS
+def test_variance_corner1():
+    assert(variance([1]) == "Error: List must have 2 or more integers")
+
+def test_variance_corner2():
+    assert(variance([]) == "Error: Empty List")
+
+# GENERAL STANDARD DEVIATION TESTS
+def test_standard_deviation_basic1():
+    assert(standard_deviation([10, 20, 30, 40]) == (math.sqrt(125)))
+
+def test_standard_deviation_basic2():
+    assert(standard_deviation([600, 470, 170, 430, 300]) == (math.sqrt(21704)))
+
+# CORNER STANDARD DEVIATION TESTS
+def test_standard_deviation_corner1():
+    assert(standard_deviation([1]) == "Error: List must have 2 or more integers")
+
+def test_standard_deviatione_corner2():
+    assert(standard_deviation([]) == "Error: Empty List")
